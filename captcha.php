cbeb2f91dc320b44490de1f74f9c492d1e949613
Abir Hossain <131145953+AbirHossainDevOps@users.noreply.github.com>
2023-05-22 17:04:16 +0600

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -9,5 +9,4 @@ $background_color = imagecolorallocate($im, 0, 0, 0);
 $text_color = imagecolorallocate($im, 233, 14, 91);
 imagestring($im, 1, 5, 5,  $text, $text_color);
 imagepng($im);
-imagedestroy($im);
 ?>
